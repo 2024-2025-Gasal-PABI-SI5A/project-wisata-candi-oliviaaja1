3afbad1303a9837f912f6f5197414015b197a825
- <olivia_2226240023@mhs.mdp.ac.id>
2024-11-13 10:14:51 +0700

    update coding

--- PAB/wisata_candi/lib/main.dart
+++ PAB/wisata_candi/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +9,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
